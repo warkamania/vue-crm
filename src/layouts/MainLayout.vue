7f3f565203d9809c3700238f1db0d34cb21db4e8
warkamania <87486508+warkamania@users.noreply.github.com>
2021-08-04 17:39:18 +0300

    Фиксация: commit 3

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="app-main-layout">
+<div>
+  <Loader v-if="loading"/>
+  <div class="app-main-layout" v-else>
      <Navbar @click="isOpen = !isOpen" />
 
      <Sidebar v-model="isOpen"/>
@@ -17,6 +19,7 @@
       </router-link>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
@@ -26,12 +29,14 @@ import Sidebar from '@/components/app/Sidebar'
 export default {
   name:'maine-layout',
   data:() => ({
-    isOpen: true 
+    isOpen: true,
+    loading: true 
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
      await this.$store.dispatch('fetchInfo')
     }
+    this.loading = false
   },
   components:{
     Navbar,
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
 import info from "./info"
+import category from './category'
+import record from './record'
 
 Vue.use(Vuex)
 
@@ -17,10 +19,17 @@ export default new Vuex.Store({
       state.error = null
     }
   },
+  actions:{
+   async fetchCurrency(){
+     const key = process.env.VUE_APP_FIXER
+     const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbol=USD,EUR,RUB`)
+     return await res.json()
+   }
+  },
   getters:{
     error: s => s.error
   },
   modules: {
-    auth,info
+    auth,info,category,record
   }
 })
